1a1451c600672b5f9ea2bf9dc0ab663df1c8fe83
Vladimir Sitnikov <vova_sitnikov@mail.ru>
2022-10-01 14:04:55 +0400

    это 4 версия

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1st versuin of project");
+        System.out.println("4 версия");
     }
 }
